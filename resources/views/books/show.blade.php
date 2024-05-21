e3659fad1297bab19a92ebb87c7d091d1797a23a
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-21 16:49:24 +0200

    update book show details section

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -4,8 +4,20 @@
 
 @section('content')
 <main>
-    <h1>{{ $book->title }}</h1>
-    <p>{{$book->plot}}</p>
+    <div class="card">
+        <img src="{{ $book->cover_image }}" class="card-img-top" alt="{{ $book->title }}">
+        <div class="card-body">
+            <h5 class="card-title">
+                Titolo: {{ $book->title }}
+            </h5>
+            <p class="card-text">
+                Overview: {{ $book->plot }}
+            </p>
+            <a class="btn btn-primary" href="{{route('books.show', $book->id)}}">
+                Details
+            </a>
+        </div>
+    </div>
 </main>
 
 @endsection
