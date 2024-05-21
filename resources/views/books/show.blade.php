feed08a229cc39d439ed7b2ad3362ef50741f861
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-21 17:44:48 +0200

    update show file

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -3,19 +3,25 @@
 @section('title', 'Books')
 
 @section('content')
-<main>
-    <div class="card">
-        <img src="{{ $book->cover_image }}" class="card-img-top" alt="{{ $book->title }}">
-        <div class="card-body">
-            <h5 class="card-title">
-                Titolo: {{ $book->title }}
-            </h5>
-            <p class="card-text">
-                Overview: {{ $book->plot }}
-            </p>
-            <a class="btn btn-primary" href="{{route('books.show', $book->id)}}">
-                Details
-            </a>
+<main class="bg-dark">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-4 col-lg-3 gy-3">
+                <div class="card">
+                    <img src="{{ $book->cover_image }}" class="card-img-top" alt="{{ $book->title }}">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Titolo: {{ $book->title }}
+                        </h5>
+                        <p class="card-text">
+                            Overview: {{ $book->plot }}
+                        </p>
+                        <a class="btn btn-primary" href="{{route('books.index')}}">
+                            Back to books
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </main>
--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -3,24 +3,28 @@
 @section('title', 'Movies')
 
 @section('content')
-<main>
-    <div class="card">
-        <img src="{{ $movie->image }}" class="card-img-top" alt="{{ $movie->title }}">
-        <div class="card-body">
-            <h5 class="card-title">
-                Titolo: {{ $movie->title }}
-            </h5>
-            <p class="card-text">
-                Titolo originale: {{ $movie->original_title }}
-            </p>
-            <div>
-                {{ $movie->vote}}
+<main class="bg-dark">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-4 col-lg-3 gy-3">
+                <div class="card">
+                    <img src="{{ $movie->image }}" class="card-img-top" alt="{{ $movie->title }}">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Titolo: {{ $movie->title }}
+                        </h5>
+                        <p class="card-text">
+                            Titolo originale: {{ $movie->original_title }}
+                        </p>
+                        <div>
+                            {{ $movie->vote}}
+                        </div>
+                        <a class="btn btn-primary" href="{{route('movies.index')}}">
+                            Back to movies
+                        </a>
+                    </div>
+                </div>
             </div>
-            <a class="btn btn-primary" href="{{route('movies.show', $movie->id)}}">
-                details
-            </a>
-        </div>
-    </div>
 </main>
 
 @endsection
