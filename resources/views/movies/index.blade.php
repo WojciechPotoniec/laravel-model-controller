c5f122b2074632f967fb012c34d98a82c6f944d3
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-21 16:49:47 +0200

    minor fix

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -8,27 +8,27 @@
     <div class="container">
         <div class="row">
             @foreach ($movies as $movie)
-                        <div class="col-12 col-md-4 col-lg-3">
-                            <div class="card">
-                                <img src="{{ $movie->image }}" class="card-img-top" alt="{{ $movie->title }}">
-                                <div class="card-body">
-                                    <h5 class="card-title">
-                                        Titolo: {{ $movie->title }}
-                                    </h5>
-                                    <p class="card-text">
-                                        Titolo origile: {{ $movie->original_title }}
-                                    </p>
-                                    <div>
-                                        {{ $movie->vote}}
-                                    </div>
-                                    <a class="btn btn-primary" href="{{route('movies.show', $movie->id)}}">
-                                        details
-                                    </a>
-                                </div>
+                <div class="col-12 col-md-4 col-lg-3 gy-3">
+                    <div class="card">
+                        <img src="{{ $movie->image }}" class="card-img-top" alt="{{ $movie->title }}">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Titolo: {{ $movie->title }}
+                            </h5>
+                            <p class="card-text">
+                                Titolo originale: {{ $movie->original_title }}
+                            </p>
+                            <div>
+                                {{ $movie->vote}}
                             </div>
+                            <a class="btn btn-primary" href="{{route('movies.show', $movie->id)}}">
+                                Details
+                            </a>
                         </div>
-                        @endforeach
                     </div>
                 </div>
+            @endforeach
+        </div>
+    </div>
 </main>
 @endsection
