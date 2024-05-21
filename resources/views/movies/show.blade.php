53182df206c46f7c2c19ecd88a23c6ff8b857227
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-21 16:50:01 +0200

    update movies show section

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -4,8 +4,23 @@
 
 @section('content')
 <main>
-    <h1>{{ $movie->title }}</h1>
-    <p>{{ $movie->plot }}</p>
+    <div class="card">
+        <img src="{{ $movie->image }}" class="card-img-top" alt="{{ $movie->title }}">
+        <div class="card-body">
+            <h5 class="card-title">
+                Titolo: {{ $movie->title }}
+            </h5>
+            <p class="card-text">
+                Titolo originale: {{ $movie->original_title }}
+            </p>
+            <div>
+                {{ $movie->vote}}
+            </div>
+            <a class="btn btn-primary" href="{{route('movies.show', $movie->id)}}">
+                details
+            </a>
+        </div>
+    </div>
 </main>
 
 @endsection
