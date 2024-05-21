174b7985f316d1f36ca7894543af68771a23a26a
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-21 16:49:01 +0200

    update books section

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -4,18 +4,28 @@
 
 @section('content')
 <main>
-    <h1>All Books</h1>
-    <ul>
-        @foreach ($books as $book)
-            <li>
-                <a href="{{route('books.show', $book->id)}}">
-                    {{ $book->title }}
-                </a>
-            </li>
-
-        @endforeach
-    </ul>
-
+    <h1>All books</h1>
+    <div class="container">
+        <div class="row">
+            @foreach ($books as $book)
+                <div class="col-12 col-md-4 col-lg-3 gy-3">
+                    <div class="card">
+                        <img src="{{ $book->cover_image }}" class="card-img-top" alt="{{ $book->title }}">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                Titolo: {{ $book->title }}
+                            </h5>
+                            <p class="card-text">
+                                Overview: {{ $book->plot }}
+                            </p>
+                            <a class="btn btn-primary" href="{{route('books.show', $book->id)}}">
+                                Details
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 </main>
-
 @endsection
