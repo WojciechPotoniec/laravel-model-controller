a5cb51591e671b7ff098e0c4521ece67a852b341
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-05-21 16:31:48 +0200

    update movie section

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -5,16 +5,30 @@
 @section('content')
 <main>
     <h1>All Movies</h1>
-    <ul>
-        @foreach ($movies as $movie)
-            <li>
-                <a href="{{route('movies.show', $movie->id)}}">
-                    {{ $movie->title }}
-                </a>
-            </li>
-        @endforeach
-    </ul>
-
+    <div class="container">
+        <div class="row">
+            @foreach ($movies as $movie)
+                        <div class="col-12 col-md-4 col-lg-3">
+                            <div class="card">
+                                <img src="{{ $movie->image }}" class="card-img-top" alt="{{ $movie->title }}">
+                                <div class="card-body">
+                                    <h5 class="card-title">
+                                        Titolo: {{ $movie->title }}
+                                    </h5>
+                                    <p class="card-text">
+                                        Titolo origile: {{ $movie->original_title }}
+                                    </p>
+                                    <div>
+                                        {{ $movie->vote}}
+                                    </div>
+                                    <a class="btn btn-primary" href="{{route('movies.show', $movie->id)}}">
+                                        details
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
+                </div>
 </main>
-
 @endsection
